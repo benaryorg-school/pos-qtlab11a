77b2c174e2800fb35e3f0c20c3be8656a3ca0bab
benaryorg <binary@benary.org>
2015-01-21 18:24:52 +0100

    added sources

---
+++ src/window.cpp
@@ -0,0 +1,32 @@
+#include <window.h>
+
+Window::Window(QWidget *parent):QWidget(parent)
+{
+	this->setWindowTitle(tr("Numbers"));
+
+	this->layout=new QHBoxLayout(this);
+	this->setLayout(this->layout);
+
+	this->llist=new QListWidget(this);
+	this->rlist=new QListWidget(this);
+	this->llist->setDragDropMode(QAbstractItemView::DragDrop);
+	this->rlist->setDragDropMode(QAbstractItemView::DragDrop);
+	this->llist->setSelectionMode(QAbstractItemView::ExtendedSelection);
+	this->rlist->setSelectionMode(QAbstractItemView::ExtendedSelection);
+	this->llist->setDefaultDropAction(Qt::MoveAction);
+	this->rlist->setDefaultDropAction(Qt::MoveAction);
+	this->layout->addWidget(this->llist);
+	this->layout->addWidget(this->rlist);
+
+	for(int i=0;i<100;i++)
+	{
+		QListWidgetItem *it=new QListWidgetItem(QString::number(i+1));
+		it->setTextAlignment(Qt::AlignRight);
+		this->llist->addItem();
+	}
+}
+
+Window::~Window(void)
+{
+}
+
---
+++ src/window.h
@@ -0,0 +1,24 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QtCore>
+#if QTCORE_VERSION>=0x050000
+#include <QtWidgets>
+#endif
+#include <QtGui>
+
+class Window:public QWidget
+{
+	Q_OBJECT
+
+public:
+	Window(QWidget *parent=0);
+	~Window(void);
+
+private:
+	QLayout *layout;
+	QListWidget *llist;
+	QListWidget *rlist;
+};
+
+#endif /** WINDOW_H **/
